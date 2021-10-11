91ded3551a2511c62b157ac7bee6a590392259e0
Khang Ly <khangtuongly@yahoo.com>
2021-10-10 17:22:20 -0700

    Add a PowerShell script to apply a theme to all sessions based on time of day

---
+++ apply-all.ps1
@@ -0,0 +1,15 @@
+﻿$light = (Get-Date -Hour 8).TimeOfDay
+$dark = (Get-Date -Hour 19).TimeOfDay
+$now = (Get-Date).TimeOfDay
+$reg_file = '.\pretty_putty_dark.reg'
+if($now -gt $light -and $now -lt $dark){
+    $reg_file = '.\pretty_putty_light.reg'
+}
+$reg_content = Get-Content $reg_file
+
+$session_names = Get-ChildItem HKCU:\Software\SimonTatham\PuTTY\Sessions\ -Name
+foreach ($session_name in $session_names)
+{
+    $reg_content -replace 'SESSION_NAME', $session_name | Out-File -Encoding default .\temp.reg
+    reg import .\temp.reg
+}
